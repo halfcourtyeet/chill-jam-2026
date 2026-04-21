8fe1e348491624580f4f9b8f21a2b5a2e84b25e4
Halfcourt Yeet <mvben33@gmail.com>
2026-04-21 15:53:53 -0700

    Moving tiles test and psuedocode instructions for the rest of the game

--- scenes/player/bullet.gd
+++ scenes/player/bullet.gd
@@ -1,11 +1,10 @@
 class_name Bullet extends Sprite2D
 
-const SPEED = 300
+const SPEED = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
